afcfa94d74ca746e44788909a52d3f53b97a1368
P CHARAN <silverspyhustles1@gmail.com>
2025-09-02 01:32:17 +0530

    Initialize API keys in auto_post.py
    Added environment variable keys for APIs in auto_post.py

---
+++ auto_post.py
@@ -0,0 +1,10 @@
+import os
+
+CRICKET_API_KEY = os.getenv("38c0464f-bf8b-48f1-b475-d53deebfe308")
+PKL_API_KEY = os.getenv("PKL_API_KEY")
+TWITTER_API_KEY = os.getenv("B2ZL590zOm4LA2yVqio0MAqkL")
+TWITTER_API_SECRET = os.getenv("YM0WuvFyeaOhDL2HKTmNkepKqBDeEGliroUOObaTJAdlCo2fX1")
+TWITTER_ACCESS_TOKEN = os.getenv("1843287108942954496-tizFJcNpa2XUTEeYkh6v1pUqeoTd9y")
+TWITTER_ACCESS_SECRET = os.getenv("OeajfP3JViSyxj8NNPys518tMawmZtXefo7TLXTZzASrP")
+
+# Your logic to fetch data, generate images, and post to Twitter/X
